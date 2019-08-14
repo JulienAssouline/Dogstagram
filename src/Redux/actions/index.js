aed3f1bb013d015919bd695de1ef72841a33f78b
JulienAssouline <julien1993@hotmail.ca>
2019-08-13 21:25:47 -0400

    remove unessacery functions

--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -1,18 +1,7 @@
-import { ADD_ARTICLE, FOUND_BAD_WORD, ADD_LIKE, REMOVE_LIKE } from "../constants/action-types"
+import { ADD_ARTICLE } from "../constants/action-types"
 
 
 export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload}
 }
 
-export function addWarning(payload) {
-  return {type: FOUND_BAD_WORD, payload}
-}
-
-export function addLike(payload) {
-  return { type: ADD_LIKE, payload }
-}
-
-export function removeLike(payload) {
-  return { type: REMOVE_LIKE, payload}
-}
